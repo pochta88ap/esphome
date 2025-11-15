96c4b34e341944b4dbdb176a31262ac76ab9016a
ap <pochta88.ap@gmail.com>
2025-11-15 18:52:45 +0300

    added CONFIG>SCHEMA

--- esphome/components/micradar/__init__.py
+++ esphome/components/micradar/__init__.py
@@ -13,3 +13,19 @@ micradar_ns = cg.esphome_ns.namespace("micradar")
 micradarComponent = micradar_ns.class_("MicradarComponent", cg.Component, uart.UARTDevice)
 
 CONF_MICRADAR_ID = "micradar_id"
+
+CONFIG_SCHEMA = cv.Schema(
+    {
+        cv.GenerateID(): cv.declare_id(micradarComponent),
+        
+    }
+)
+
+CONFIG_SCHEMA = cv.All(
+    CONFIG_SCHEMA.extend(uart.UART_DEVICE_SCHEMA).extend(cv.COMPONENT_SCHEMA)
+)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
