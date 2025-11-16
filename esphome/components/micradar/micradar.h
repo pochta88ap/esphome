8edaf272e64b034ba01c7caebdea08caa270d8c0
ap <pochta88.ap@gmail.com>
2025-11-16 19:12:04 +0300

    corrected

--- esphome/components/micradar/micradar.h
+++ esphome/components/micradar/micradar.h
@@ -47,6 +47,9 @@ static constexpr uint8_t MAX_LINE_LENGTH = 46;  // Max characters for serial buf
   void dump_config() override;
   void loop() override;
 
+  void read_all_info();
+  void restart_and_read_all_info();
+
  protected:
   void readline_(int readch);
 
@@ -56,8 +59,7 @@ static constexpr uint8_t MAX_LINE_LENGTH = 46;  // Max characters for serial buf
   uint8_t checkDigit_( uint8_t *buf, uint16_t len);
   void issue_data_( uint8_t control, uint8_t command, uint8_t *bytes, uint16_t len );
   void handle_data_();
-  void read_all_info();
-  void restart_and_read_all_info();
+  
   //issue command functions
   void issue_heartbit_package_query_();
   void issue_module_reset_();
