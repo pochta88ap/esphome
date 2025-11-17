eaad746dde5b61527656533b859fd6446c50dfb0
ap <pochta88.ap@gmail.com>
2025-11-17 22:01:29 +0300

    corrected

--- esphome/components/micradar/text_sensor.py
+++ esphome/components/micradar/text_sensor.py
@@ -12,21 +12,41 @@ from . import CONF_MICRADAR_ID, MicradarComponent
 
 DEPENDENCIES = ["micradar"]
 
+CONF_PRODUCT_MODEL = "product_model"
+CONF_PRODUCT_ID = "product_id"
+CONF_HARDWARE_MODEL = "hardware_model"
+CONF_FIRMWARE_VERSION = "hardware_version"
+
 CONFIG_SCHEMA = {
     cv.GenerateID(CONF_MICRADAR_ID): cv.use_id(MicradarComponent),
-    cv.Optional(CONF_VERSION): text_sensor.text_sensor_schema(
+    cv.Optional(CONF_PRODUCT_MODEL): text_sensor.text_sensor_schema(
+        entity_category=ENTITY_CATEGORY_DIAGNOSTIC, icon=ICON_CHIP
+    ),
+     cv.Optional(CONF_PRODUCT_ID): text_sensor.text_sensor_schema(
+        entity_category=ENTITY_CATEGORY_DIAGNOSTIC, icon=ICON_CHIP
+    ),
+    cv.Optional(CONF_HARDWARE_MODEL): text_sensor.text_sensor_schema(
+        entity_category=ENTITY_CATEGORY_DIAGNOSTIC, icon=ICON_CHIP
+    ),
+    cv.Optional(CONF_FIRMWARE_VERSION): text_sensor.text_sensor_schema(
         entity_category=ENTITY_CATEGORY_DIAGNOSTIC, icon=ICON_CHIP
     ),
     
+    
 }
-CONF_PRODUCT_MODEL = "product_model"
-CONF_PRODUCT_ID = "product_id"
+
 async def to_code(config):
     micradar_component = await cg.get_variable(config[CONF_MICRADAR_ID])
     if product_model_config := config.get(CONF_PRODUCT_MODEL):
         sens = await text_sensor.new_text_sensor(product_model_config)
-        cg.add(micradar_component.set_version_text_sensor(sens))
+        cg.add(micradar_component.set_product_model_text_sensor(sens))
     if product_id_config := config.get(CONF_PRODUCT_ID):
         sens = await text_sensor.new_text_sensor(product_id_config)
-        cg.add(micradar_component.set_version_text_sensor(sens))
+        cg.add(micradar_component.set_product_id_text_sensor(sens))
+    if hardware_model_config := config.get(CONF_HARDWARE_MODEL):
+        sens = await text_sensor.new_text_sensor(hardware_model_config)
+        cg.add(micradar_component.set_hardware_model_text_sensor(sens))
+    if firmware_version_config := config.get(CONF_FIRMWARE_VERSION):
+        sens = await text_sensor.new_text_sensor(product_id_config)
+        cg.add(micradar_component.set_firmware_version_text_sensor(sens))
     
