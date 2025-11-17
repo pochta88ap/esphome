36a94e5324bde88cafa8c87d75c5ee276b2ec830
ap <pochta88.ap@gmail.com>
2025-11-17 21:04:31 +0300

    text_sensor

--- esphome/components/micradar/text_sensor.py
+++ esphome/components/micradar/text_sensor.py
@@ -22,7 +22,10 @@ CONFIG_SCHEMA = {
 
 async def to_code(config):
     micradar_component = await cg.get_variable(config[CONF_MICRADAR_ID])
-    if version_config := config.get(CONF_VERSION):
-        sens = await text_sensor.new_text_sensor(version_config)
+    if product_model_config := config.get(CONF_VERSION):
+        sens = await text_sensor.new_text_sensor(product_model_config)
+        cg.add(micradar_component.set_version_text_sensor(sens))
+    if product_id_config := config.get(CONF_VERSION):
+        sens = await text_sensor.new_text_sensor(product_id_config)
         cg.add(micradar_component.set_version_text_sensor(sens))
     
