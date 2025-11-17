5d47ac361f6fc9dcebd33e2063b348666c36da95
ap <pochta88.ap@gmail.com>
2025-11-17 22:18:26 +0300

    corrected

--- esphome/components/micradar/micradar.cpp
+++ esphome/components/micradar/micradar.cpp
@@ -221,7 +221,7 @@ namespace micradar {
                     case CMD_PRODUCT_MODEL_QUERY:
 #ifdef USE_TEXT_SENSOR
                         if (this->product_model_text_sensor_ != nullptr) {
-                            ESP_LOGV(TAG, "product model: %s ", format_hex_pretty(this->buffer_data_+6, this->buffer_data +5 ).c_str());
+                            ESP_LOGV(TAG, "product model: %s ", format_hex_pretty(this->buffer_data_+6, this->buffer_data_[5] ).c_str());
                       //      this->product_model_text_sensor_->publish_state(product_info);
                         }
 #endif
