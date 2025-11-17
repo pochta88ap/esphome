c49d93d12b55dde909cb44406b0b604aa1dd7cc0
ap <pochta88.ap@gmail.com>
2025-11-17 22:50:41 +0300

    corrected

--- esphome/components/micradar/micradar.cpp
+++ esphome/components/micradar/micradar.cpp
@@ -221,7 +221,7 @@ namespace micradar {
                     case CMD_PRODUCT_MODEL_QUERY:
 #ifdef USE_TEXT_SENSOR
                         if (this->product_model_text_sensor_ != nullptr) {
-                            product_model_ = new std::string((char*)(this->buffer_data_+6));
+                            product_model_ = ((char*)(this->buffer_data_+6));
                             ESP_LOGV(TAG, "product model: %s", product_info.c_str());
                             this->product_model_text_sensor_->publish_state(product_model_);
                         }
@@ -230,7 +230,7 @@ namespace micradar {
                     case CMD_PRODUCT_ID_QUERY:
 #ifdef USE_TEXT_SENSOR
                         if (this->product_id_text_sensor_ != nullptr) {
-                            product_id_ = new std::string((char*)(this->buffer_data_+6));
+                            product_id_ = ((char*)(this->buffer_data_+6));
                             ESP_LOGV(TAG, "product id: %s", product_id_.c_str());
                             this->product_id_text_sensor_->publish_state(product_id);
                         }
@@ -239,7 +239,7 @@ namespace micradar {
                     case CMD_HARDWARE_MODEL_QUERY:
 #ifdef USE_TEXT_SENSOR
                         if (this->hardware_model_text_sensor_ != nullptr) {
-                            hardware_model_ = new std::string((char*)(this->buffer_data_+6));
+                            hardware_model_ = ((char*)(this->buffer_data_+6));
                             ESP_LOGV(TAG, "hardware model: %s", hardware_model_.c_str());
                             this->hardware_model_text_sensor_->publish_state(hardware_model_);
                         }
@@ -248,7 +248,7 @@ namespace micradar {
                     case CMD_FIRMWARE_VERSION_QUERY:
 #ifdef USE_TEXT_SENSOR
                         if (this->firmware_version_text_sensor_ != nullptr) {
-                            firmware_version_ = new std::string((char*)(this->buffer_data_+6));
+                            firmware_version_ = ((char*)(this->buffer_data_+6));
                             ESP_LOGV(TAG, "firmware_version: %s", firmware_version_.c_str());
                             this->firmware_version_text_sensor_->publish_state(firmware_version_);
                         }
