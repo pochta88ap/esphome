5c2b9777e501746cc4b859c6387462eee7b0f591
ap <pochta88.ap@gmail.com>
2026-06-04 01:11:53 +0300

    filter

--- esphome/components/micradar/binary_sensor.py
+++ esphome/components/micradar/binary_sensor.py
@@ -23,17 +23,17 @@ CONFIG_SCHEMA = {
     cv.GenerateID(CONF_MICRADAR_ID): cv.use_id(MicradarComponent),
     cv.Optional(CONF_HAS_TARGET): binary_sensor.binary_sensor_schema(
         device_class=DEVICE_CLASS_OCCUPANCY,
-        filters=[{"settle": cv.TimePeriod(milliseconds=200)}],
+        #filters=[{"settle": cv.TimePeriod(milliseconds=200)}],
         icon=ICON_ACCOUNT,
     ),
     cv.Optional(CONF_HAS_MOVING_TARGET): binary_sensor.binary_sensor_schema(
         device_class=DEVICE_CLASS_MOTION,
-        filters=[{"settle": cv.TimePeriod(milliseconds=200)}],
+        #filters=[{"settle": cv.TimePeriod(milliseconds=200)}],
         icon=ICON_MOTION_SENSOR,
     ),
     cv.Optional(CONF_HAS_STILL_TARGET): binary_sensor.binary_sensor_schema(
         device_class=DEVICE_CLASS_OCCUPANCY,
-        filters=[{"settle": cv.TimePeriod(milliseconds=200)}],
+        #filters=[{"settle": cv.TimePeriod(milliseconds=200)}],
         icon=ICON_MOTION_SENSOR,
     ),
 }
