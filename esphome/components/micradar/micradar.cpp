5a84b21623b0b644ab60fe66da2a3f7b38cb4f3c
ap <pochta88.ap@gmail.com>
2026-06-03 23:50:46 +0300

    INFO

--- esphome/components/micradar/micradar.cpp
+++ esphome/components/micradar/micradar.cpp
@@ -443,9 +443,9 @@ namespace micradar {
                         uint16_t px, py, d;
                         float angle = 0;
                         num_targets_ = dataLength/TRACK_DATA_LENGTH;
-                        ESP_LOGD(TAG, "targets: %d ", num_targets_ );
+                        ESP_LOGI(TAG, "targets: %d ", num_targets_ );
                         for( int pos = 0; pos < MAX_TARGETS; pos++ ){
-                            if( pos >=num_targets_) {
+                            if( pos >num_targets_) {
                                 SAFE_PUBLISH_SENSOR_UNKNOWN(this->x_coord_sensors_[pos]);
                                 SAFE_PUBLISH_SENSOR_UNKNOWN(this->y_coord_sensors_[pos]);
                                 SAFE_PUBLISH_SENSOR_UNKNOWN(this->dist_sensors_[pos]);
@@ -482,7 +482,7 @@ namespace micradar {
                             SAFE_PUBLISH_SENSOR(this->dist_sensors_[targets_[pos].index-1], targets_[pos].distance);
                             SAFE_PUBLISH_SENSOR(this->move_energy_sensors_[targets_[pos].index-1], targets_[pos].size); 
                             SAFE_PUBLISH_SENSOR(this->angle_sensors_[targets_[pos].index-1], angle); 
-                            ESP_LOGD(TAG, "Tracking info: Index: %d size: %d characteristics: %d x: %d \n y: %d height: %d velocity: %d distance: %d angle: %f", 
+                            ESP_LOGI(TAG, "Tracking info: Index: %d size: %d characteristics: %d x: %d \n y: %d height: %d velocity: %d distance: %d angle: %f", 
                                     targets_[pos].index, targets_[pos].size, targets_[pos].characteristics, 
                                     targets_[pos].x, targets_[pos].y, targets_[pos].height, targets_[pos].velocity, targets_[pos].distance, angle);
                         }
