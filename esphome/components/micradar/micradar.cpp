4c06fc9f2c51499335e3c4bfc4607c70617ce476
ap <pochta88.ap@gmail.com>
2026-06-04 00:07:35 +0300

    pos

--- esphome/components/micradar/micradar.cpp
+++ esphome/components/micradar/micradar.cpp
@@ -445,7 +445,7 @@ namespace micradar {
                         num_targets_ = dataLength/TRACK_DATA_LENGTH;
                         ESP_LOGI(TAG, "targets: %d ", num_targets_ );
                         for( int pos = 0; pos < MAX_TARGETS; pos++ ){
-                            if( pos >num_targets_) {
+                            if( pos >=num_targets_) {
                                 SAFE_PUBLISH_SENSOR_UNKNOWN(this->x_coord_sensors_[pos]);
                                 SAFE_PUBLISH_SENSOR_UNKNOWN(this->y_coord_sensors_[pos]);
                                 SAFE_PUBLISH_SENSOR_UNKNOWN(this->dist_sensors_[pos]);
