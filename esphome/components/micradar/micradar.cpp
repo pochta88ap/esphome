b95c8fdb4c84dcb6bd1470c531d99fc8b1ebb4d7
ap <pochta88.ap@gmail.com>
2026-06-05 22:12:14 +0300

    update

--- esphome/components/micradar/micradar.cpp
+++ esphome/components/micradar/micradar.cpp
@@ -635,7 +635,7 @@ void MicradarComponent::set_angle_sensor(uint8_t target, sensor::Sensor *s) {
   this->angle_sensors_[target].set_sensor(s);
 }
 void MicradarComponent::set_track_frequency( float_t f ) {
-    this->track_frequency = f;
+    this->track_frequency_ = f;
 }
 
 void MicradarComponent::set_sensors_unknown( uint8_t tgt ){
