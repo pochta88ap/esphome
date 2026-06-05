21a6c99d57c73b2ad65da9e2a4cc9bbcd2180e1c
ap <pochta88.ap@gmail.com>
2026-06-05 22:10:27 +0300

    update

--- esphome/components/micradar/number/track_frequency_number.cpp
+++ esphome/components/micradar/number/track_frequency_number.cpp
@@ -4,9 +4,9 @@ namespace esphome::micradar {
 
 TrackFrequencyNumber::TrackFrequencyNumber(float_t frequency) : frequency_(frequency) {}
 
-void GateThresholdNumber::control(float value) {
+void TrackFrequencyNumber::control(float value) {
   this->publish_state(value);
   this->parent_->set_track_frequency(this->frequency_);
 }
 
-}  // namespace esphome::ld2410
+}  // namespace esphome::micradar
